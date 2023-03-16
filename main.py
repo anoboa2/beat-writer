91e305889f38e809d7388c7b05c3975ee11d1e33
Alex Noboa <55636642+anoboa2@users.noreply.github.com>
2023-03-16 15:25:41 -0400

    partial implement of gpt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import openai
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -15,8 +16,23 @@ async def on_ready():
 
 @bot.command(aliases=['hi'])
 async def write(ctx, *names):
-  print(ctx.author, ctx.message.content)
-  await ctx.send(f'Hello {names}!')
+    ingredients_string = " ".join(names)
+
+    # Make API call to GPT for recipe
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Please suggest a chicken-based recipe that includes {names}.",
+        max_tokens=2048,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # Get recipe from API response
+    recipe = response.choices[0].text.strip()
+
+    # Send recipe to Discord channel
+    await ctx.send(f"Here's a recipe that includes {ingredients_string}: {recipe}")
 
 @bot.command()
 async def ping(ctx):
